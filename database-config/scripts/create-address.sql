a7b3d1fe175178cf0c04d701df938b4a92ffccbc
Guilherme Gules <guilhermegules@gmail.com>
2022-09-17 12:06:55 -0300

    chore: removing unused scripts

--- database-config/scripts/create-address.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE address (
-	address_id VARCHAR(36) NOT NULL PRIMARY KEY,
-	city VARCHAR(50) NOT NULL,
-	neighborhood VARCHAR(100) NOT NULL,
-	state VARCHAR(50) NOT NULL,
-	cep VARCHAR(8) NOT NULL,
-	number varchar(8) NOT NULL,
-	additional_info VARCHAR(100) NULL
-);
--- database-config/scripts/insert-address.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO bdudb.address
-(address_id, city, neighborhood, street, state, cep, `number`, additional_info, user_id)
-VALUES('7adc7be7-12ad-4205-815d-25d0f55a3757', 'Dourados', 'Parque Alvorada', 'Rua Saturno', 'MS', '79823398', '9802', 'Endereço principal', 'b80120ca-eeaa-4bd6-bea1-b8f42e3a66ef');
