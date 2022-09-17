8ef25465e21a6eb52fcc35df26405e4d44ac2a3a
Guilherme Gules <guilhermegules@gmail.com>
2022-09-17 13:13:45 -0300

    feat: add the initial login screen

--- bdu-app/src/app/modules/user/user.module.ts
+++ bdu-app/src/app/modules/user/user.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './containers/login/login.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [CommonModule, UserRoutingModule],
+  declarations: [LoginComponent],
+  imports: [CommonModule, UserRoutingModule, MatCardModule, MatButtonModule, MatInputModule],
 })
 export class UserModule {}
