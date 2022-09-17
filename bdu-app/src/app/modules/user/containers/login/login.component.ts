8d95668022c476e0f95c89bd8638fbfcce5171a3
Guilherme Gules <guilhermegules@gmail.com>
2022-09-17 13:24:37 -0300

    feat: adding reactive forms in login view

--- bdu-app/src/app/modules/user/containers/login/login.component.ts
+++ bdu-app/src/app/modules/user/containers/login/login.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public formGroup = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  public onLogin() {
+    console.log(this.formGroup.value);
+  }
+
+  public redirectToRegister() {
+    this.router.navigate(['register']);
+  }
+}
