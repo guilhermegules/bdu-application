5dc7e990f74422ad1720fd472196f4f4eaf903dc
Guilherme Gules <guilhermegules@gmail.com>
2022-09-17 13:25:12 -0300

    feat: creating the register component

--- bdu-app/src/app/modules/user/user-routing.module.ts
+++ bdu-app/src/app/modules/user/user-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './containers/login/login.component';
+import { RegisterComponent } from './containers/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
 ];
 
--- bdu-app/src/app/modules/user/user.module.ts
+++ bdu-app/src/app/modules/user/user.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './containers/login/login.component';
+import { RegisterComponent } from './containers/register/register.component';
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [CommonModule, UserRoutingModule, MatCardModule, MatButtonModule, MatInputModule],
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, UserRoutingModule, MatCardModule, MatButtonModule, MatInputModule, ReactiveFormsModule],
 })
 export class UserModule {}
