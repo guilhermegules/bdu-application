580743d9a13a828879397f9fde69c2cf2e232c44
guilhermegules <guilhermegules@gmail.com>
2022-09-21 21:14:45 -0300

    feat: creating register view

--- bdu-app/src/app/modules/user/containers/register/register.component.ts
+++ bdu-app/src/app/modules/user/containers/register/register.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  public formGroup = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    name: ['', [Validators.required]],
+    cpf: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  public onRegister() {
+    console.log(this.formGroup.value);
+  }
+
+  public redirectToLogin() {
+    this.router.navigate(['/']);
+  }
+}
