8d2f4442263d2f767cf66a1c699a75f690f271e0
Guilherme Gules <guilhermegules@gmail.com>
2022-09-24 16:10:24 -0300

    feat: using the auth service to authenticate user

--- bdu-app/src/app/modules/user/user.module.ts
+++ bdu-app/src/app/modules/user/user.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './containers/login/login.component';
 import { RegisterComponent } from './containers/register/register.component';
+import { UserDashboardComponent } from './containers/user-dashboard/user-dashboard.component';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, UserRoutingModule, MatCardModule, MatButtonModule, MatInputModule, ReactiveFormsModule],
+  declarations: [LoginComponent, RegisterComponent, UserDashboardComponent],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+  ],
 })
 export class UserModule {}
