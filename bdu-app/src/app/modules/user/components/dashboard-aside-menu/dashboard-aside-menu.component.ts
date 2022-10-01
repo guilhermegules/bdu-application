6d6c32da80ee420dd837985ac3e0a71ba1003e69
guilhermegules <guilhermegules@gmail.com>
2022-10-01 17:33:20 -0300

    feat(user-dashboard): creating dashboard structure

---
+++ bdu-app/src/app/modules/user/components/dashboard-aside-menu/dashboard-aside-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-aside-menu',
+  templateUrl: './dashboard-aside-menu.component.html',
+  styleUrls: ['./dashboard-aside-menu.component.scss'],
+})
+export class DashboardAsideMenuComponent {}
--- bdu-app/src/app/modules/user/user-routing.module.ts
+++ bdu-app/src/app/modules/user/user-routing.module.ts
@@ -4,11 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '@core/guards/auth.guard';
 import { LoginComponent } from './containers/login/login.component';
 import { RegisterComponent } from './containers/register/register.component';
+import { UserContainerComponent } from './containers/user-container/user-container.component';
 import { UserDashboardComponent } from './containers/user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
     component: LoginComponent,
   },
   {
@@ -17,8 +23,14 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    component: UserDashboardComponent,
-    canActivate: [AuthGuard],
+    component: UserContainerComponent,
+    children: [
+      {
+        path: '',
+        component: UserDashboardComponent,
+        canActivate: [AuthGuard],
+      },
+    ],
   },
 ];
 
