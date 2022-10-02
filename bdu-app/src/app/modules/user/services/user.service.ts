2a1677c4e6371db44c9302496e76a60c16aa25a3
guilhermegules <guilhermegules@gmail.com>
2022-10-02 19:02:48 -0300

    feat: show the account information on the dashboard

--- bdu-app/src/app/modules/user/services/user.service.ts
+++ bdu-app/src/app/modules/user/services/user.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { environment } from '@environments/environment';
-import { IRegisterUser } from '../models/user.model';
+import { IRegisterUser, IUserAccount } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +13,9 @@ export class UserService {
 
   public register(user: IRegisterUser): Observable<IRegisterUser> {
     return this.http.post<IRegisterUser>(`${environment.BASE_URL}/user`, user);
+  }
+
+  public getUserById(id: string): Observable<IUserAccount> {
+    return this.http.get<IUserAccount>(`${environment.BASE_URL}/user/${id}`);
   }
 }
