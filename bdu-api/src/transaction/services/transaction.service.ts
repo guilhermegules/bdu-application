9845a179fe505c8f8104fbec92e39914af85f31c
guilhermegules <guilhermegules@gmail.com>
2022-10-04 21:29:34 -0300

    refactor: change the create transaction implementation to use the services instead of repositories

--- bdu-api/src/transaction/services/transaction.service.ts
+++ bdu-api/src/transaction/services/transaction.service.ts
@@ -5,20 +5,16 @@ import { Repository } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
 import { CreateTransactionDto } from '../dto/create-transaction.dto';
-import { TransactionAggregation } from '../entities/transaction-aggregation.entity';
 import { Transaction } from '../entities/transaction.entity';
-import { BankAccount } from '../../bank-account/entities/bank-account.entity';
 import { BankAccountService } from './../../bank-account/services/bank-account.service';
+import { TransactionAggregationService } from './transaction-aggregation.service';
 
 @Injectable()
 export class TransactionService {
   constructor(
     @InjectRepository(Transaction)
     private transactionRepository: Repository<Transaction>,
-    @InjectRepository(TransactionAggregation)
-    private transactionAggregationRepository: Repository<TransactionAggregation>,
-    @InjectRepository(BankAccount)
-    private bankAccountRepository: Repository<BankAccount>,
+    private transactionAggregationService: TransactionAggregationService,
     private bankAccountService: BankAccountService,
   ) {}
 
@@ -28,14 +24,6 @@ export class TransactionService {
       type: createTransactionDto.type,
       transactionId: uuidv4(),
     });
-
-    const transactionAggregation = this.transactionAggregationRepository.create(
-      {
-        transactionReceiver: createTransactionDto.transactionReceiver,
-        transactionSender: createTransactionDto.transactionSender,
-        transactionId: transaction.transactionId,
-      },
-    );
 
     const [senderAccount, receiverAccount] = await Promise.all([
       this.bankAccountService.findOne(createTransactionDto.transactionSender),
@@ -52,25 +40,27 @@ export class TransactionService {
     receiverAccount.balance += createTransactionDto.amount;
 
     await Promise.all([
-      this.bankAccountRepository.update(
-        { accountNumber: createTransactionDto.transactionSender },
-        senderAccount,
-      ),
-      this.bankAccountRepository.update(
-        { accountNumber: createTransactionDto.transactionReceiver },
-        receiverAccount,
-      ),
+      this.bankAccountService.update({
+        accountNumber: senderAccount.accountNumber,
+        balance: senderAccount.balance,
+      }),
+      this.bankAccountService.update({
+        accountNumber: receiverAccount.accountNumber,
+        balance: receiverAccount.balance,
+      }),
       this.transactionRepository.insert(transaction),
-      this.transactionAggregationRepository.insert(transactionAggregation),
+      this.transactionAggregationService.create({
+        transactionReceiver: createTransactionDto.transactionReceiver,
+        transactionSender: createTransactionDto.transactionSender,
+        transactionId: transaction.transactionId,
+      }),
     ]);
 
     return {
       senderAccount: {
-        balance: senderAccount.balance,
         accountNumber: senderAccount.accountNumber,
       },
       receiverAccount: {
-        balance: receiverAccount.balance,
         accountNumber: receiverAccount.accountNumber,
       },
       transaction,
--- bdu-api/src/transaction/transaction.module.ts
+++ bdu-api/src/transaction/transaction.module.ts
@@ -4,20 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionService } from './services/transaction.service';
 import { TransactionController } from './controllers/transaction.controller';
 import { Transaction } from './entities/transaction.entity';
-import { BankAccount } from './../bank-account/entities/bank-account.entity';
 import { TransactionAggregation } from './entities/transaction-aggregation.entity';
 import { BankAccountModule } from './../bank-account/bank-account.module';
+import { TransactionAggregationService } from './services/transaction-aggregation.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      Transaction,
-      TransactionAggregation,
-      BankAccount,
-    ]),
+    TypeOrmModule.forFeature([Transaction, TransactionAggregation]),
     BankAccountModule,
   ],
   controllers: [TransactionController],
-  providers: [TransactionService],
+  providers: [TransactionService, TransactionAggregationService],
 })
 export class TransactionModule {}
