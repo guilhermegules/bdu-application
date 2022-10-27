59aea8d4b7de26b8955d32eb67a3417e24ddb069
Guilherme Gules <guilhermegules@gmail.com>
2022-10-26 21:50:08 -0300

    feat: create service to query all the transactions of the account

--- bdu-api/src/transaction/services/transaction.service.ts
+++ bdu-api/src/transaction/services/transaction.service.ts
@@ -1,3 +1,5 @@
+import { BankAccount } from './../../bank-account/entities/bank-account.entity';
+import { TransactionAggregation } from './../entities/transaction-aggregation.entity';
 import { BusinessException } from './../../core/exceptions/business.exception';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,6 +10,7 @@ import { CreateTransactionDto } from '../dto/create-transaction.dto';
 import { Transaction } from '../entities/transaction.entity';
 import { BankAccountService } from './../../bank-account/services/bank-account.service';
 import { TransactionAggregationService } from './transaction-aggregation.service';
+import { from } from 'rxjs';
 
 @Injectable()
 export class TransactionService {
@@ -69,7 +72,22 @@ export class TransactionService {
   }
 
   async findAllByAccountNumber(accountNumber: string) {
-    return [];
+    const transactionsByAccountNumber = await this.transactionRepository
+      .createQueryBuilder('transaction')
+      .innerJoin(
+        TransactionAggregation,
+        'transaction_aggregation',
+        'transaction.transaction_id = transaction_aggregation.transaction_id',
+      )
+      .innerJoin(
+        BankAccount,
+        'account',
+        'account.account_number = transaction_aggregation.transaction_sender OR account.account_number = transaction_aggregation.transaction_receiver',
+      )
+      .where('account.account_number = :accountNumber', { accountNumber })
+      .getMany();
+
+    return transactionsByAccountNumber;
   }
 
   findOne(transactionId: string) {
